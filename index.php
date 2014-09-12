0111b284114ffddde50a4ebf915240e8c3902ec8
Tom Duffy <tom@techremedy.net>
2014-09-12 16:01:14 -0700

    added empty functions to classes and started homepage content

--- index.php
+++ index.php
@@ -2,8 +2,21 @@
 
 require_once('init.php');
 
-print('<pre>');
-var_dump($_SESSION);
-print('</pre>');
-die();
 ?>
+
+<?php require_once('header.php'); ?>
+
+<div id="mainButtons">
+  <button>Tie a Fly</button>
+  <button>Manage Material Inventory</button>
+</div>
+
+<div id="tiedFlies">
+  <h3>Previous Flies</h3>
+</div>
+
+<div id="materialInventory">
+  <h3>Material Inventory</h3>
+</div>
+
+<?php require_once('footer.php'); ?>
