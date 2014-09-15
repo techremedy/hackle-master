ce19885a375adf025aebd1837830738b35e738f3
Tom Duffy <tom@techremedy.net>
2014-09-15 10:23:44 -0700

    starting to build inventory system

---
+++ inventory.php
@@ -0,0 +1,134 @@
+<?php
+
+?>
+
+<?php require_once('header.php'); ?>
+
+<div id="hookInventory">
+  <table>
+    <thead>
+      <th>ID</th>
+      <th>Size</th>
+      <th>Brand</th>
+      <th>Model</th>
+      <th>Type</th>
+    </thead>
+    <tbody>
+
+    </tbody>
+  </table>
+</div>
+
+<div id="dubbingInventory">
+  <table>
+    <thead>
+      <th>ID</th>
+      <th>Brand</th>
+      <th>Natural/Synthetic</th>
+      <th>Type</th>
+      <th>Color</th>
+      <th>Qty</th>
+    </thead>
+    <tbody>
+
+    </tbody>
+  </table>
+</div>
+
+<div id="hackleInventory">
+  <table>
+    <thead>
+      <th>ID</th>
+      <th>Brand</th>
+      <th>Sex</th>
+      <th>Length</th>
+      <th>Color</th>
+      <th>Qty</th>
+    </thead>
+    <tbody>
+
+    </tbody>
+  </table>
+</div>
+
+<div id="otherFeatherInventory">
+  <table>
+    <thead>
+      <th>ID</th>
+      <th>Brand</th>
+      <th>Bird</th>
+      <th>Feather</th>
+      <th>Size</th>
+      <th>Color</th>
+      <th>Qty</th>
+    </thead>
+    <tbody>
+
+    </tbody>
+  </table>
+</div>
+
+<div id="threadInventory">
+  <table>
+    <thead>
+      <th>ID</th>
+      <th>Brand</th>
+      <th>Size</th>
+      <th>Color</th>
+      <th>Qty</th>
+    </thead>
+    <tbody>
+
+    </tbody>
+  </table>
+</div>
+
+<div id="beadInventory">
+  <table>
+    <thead>
+      <th>ID</th>
+      <th>Brand</th>
+      <th>Type</th>
+      <th>Material</th>
+      <th>Size</th>
+      <th>Qty</th>
+    </thead>
+    <tbody>
+
+    </tbody>
+  </table>
+</div>
+
+<div id="hairInventory">
+  <table>
+    <thead>
+      <th>ID</th>
+      <th>Brand</th>
+      <th>Animal</th>
+      <th>Part</th>
+      <th>Color</th>
+      <th>Qty</th>
+    </thead>
+    <tbody>
+
+    </tbody>
+  </table>
+</div>
+
+<div id="miscInventory">
+  <table>
+    <thead>
+      <th>ID</th>
+      <th>Brand</th>
+      <th>Category</th>
+      <th>Size</th>
+      <th>Color</th>
+      <th>Qty</th>
+    </thead>
+    <tbody>
+
+    </tbody>
+  </table>
+</div>
+
+<?php require_once('footer.php'); ?>
