6ed034f3418ccc7ac8917f8738dc68951b8ddec9
Tom Duffy <tom@techremedy.net>
2014-09-26 12:44:13 -0700

    got bootstrap all working

--- header.php
+++ header.php
@@ -5,8 +5,32 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>HackleMaster - Fly Tying Log and Material Inventory</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/hackle-master.css">
   </head>
   <body>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle Navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">HackleMaster</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php">Home</a></li>
+            <li><a href="newFly.php">Tie A Fly</a></li>
+            <li><a href="inventory.php">Manage Materials</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
     <div class="container">
--- index.php
+++ index.php
@@ -6,11 +6,6 @@ require_once('init.php');
 
 <?php require_once('header.php'); ?>
 
-<div id="mainButtons">
-  <a href="newFly.php">Tie a Fly</a>
-  <a href="inventory.php">Manage Material Inventory</a>
-</div>
-
 <div id="tiedFlies">
   <h3>Previous Flies</h3>
 </div>
