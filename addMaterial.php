94ec269c2678a87e6ea9192d42463940a1369843
Tom Duffy <tom@techremedy.net>
2014-10-02 16:51:53 -0700

    updated the addMaterial page

--- addMaterial.php
+++ addMaterial.php
@@ -7,28 +7,15 @@ if(isset($_POST['submitted'])){
   $form = $_POST;
   $addMaterial = $materialsObj->addMaterial($form);
 }
-
-if(!is_numeric($category)){
-  //redirect non-numerics back to index
-  header("Location: index.php");
-  die();
-}
-
-//fetch appropriate form for category
-$form = $materialsObj->getAddMaterialForm($category);
-
 ?>
 
 <?php require_once('header.php'); ?>
 
 <form action="" method="post">
   
-  <?php print($form); ?>
-
   <input type="hidden" name="submitted" value="true">
   <input type="hidden" name="user" value="<?php print($_SESSION['user']['id']);?>">
-  <input type="hidden" name="category" value="<?php print($category);?>">
-  <input type="submit" value="Add Material">
+  <button type="submit" class="btn btn-primary">Add Material</button>
 </form>
 
 <?php require_once('footer.php'); ?>
